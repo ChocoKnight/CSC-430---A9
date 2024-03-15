76086449a0900face92d8177d48a2d77ffc2966f
Kevin Tan <k.tann.578@gmail.com>
2024-03-15 05:45:01 -0700

    Update OAZO-A9.vb
    interp of num str ids

--- OAZO-A9.vb
+++ OAZO-A9.vb
@@ -104,11 +104,14 @@ Module VBModule
         Inherits Value
         Public args As List(Of String)
         Public body As ExprC 
+        Public env as Environment
 
         Public Sub New(ByVal arguments As List(Of String),
-                        ByVal body_new As ExprC)
+                        ByVal body_new As ExprC,
+                        ByVal env_new As Environment)
             args = arguments
             body = body_new
+            env = env_new
         End Sub
     End Class
 
@@ -157,24 +160,105 @@ Module VBModule
     End Sub
 
     Function Serialize(val as Value) As String
-        If val.GetType() = GetType(NumV) Then 
+        If TypeOf val Is NumV Then 
             Return "" & TryCast(val, NumV).num
-        ElseIf val.GetType() = GetType(StrV) Then
+        ElseIf TypeOf val Is StrV Then
             Return "" & TryCast(val, StrV).str
-        ElseIf val.GetType() = GetType(BoolV) Then
+        ElseIf TypeOf val Is BoolV Then
             If (TryCast(val, BoolV)).bool Then
                 Return "true"
             Else 
                 Return "false"
-            End IF
-        ElseIf val.GetType() = GetType(CloV) Then
+            End If
+        ElseIf TypeOf val Is CloV Then
             Return "#<procedure>"
-        ElseIf val.GetType() = GetType(PrimV) Then
+        ElseIf TypeOf val Is PrimV Then
             Return "#<primop>"
         End If
     End Function
 
-    Function Interp(ast as ExprC, env as Environment)
+    Function Interp(ast as ExprC, env as Environment) As Value
+        If TypeOf ast Is NumC Then 
+            Return New NumV(DirectCast(ast, NumC).num)
+        ElseIf TypeOf ast Is IdC Then
+            Return Lookup(DirectCast(ast, IdC).id, env)
+        ElseIf TypeOf ast Is StrC Then
+            Return New StrV(DirectCast(ast, StrC).str)
+        ElseIf TypeOf ast Is IfC Then
+            Dim if_statement As IfC = DirectCast(ast, IfC)
+            Dim if_cond As Value = Interp(if_statement.cond, env)
+            If TypeOf if_cond Is BoolV Then
+                If DirectCast(if_cond, BoolV).bool Then
+                    Return Interp(if_statement.then_case, env)
+                Else 
+                    Return Interp(if_statement.else_case, env)
+                End If
+            Else
+                Throw New System.Exception("Interp - Condition was not a boolean")
+            End If
+        ElseIf TypeOf ast Is LamC Then
+            Dim anon As LamC = TryCast(ast, LamC)
+            Return New CloV(anon.ids, anon.body, env)
+        ElseIf TypeOf ast Is AppC Then
+            Dim func As Value = Interp(ast, env)
+            If TypeOf func Is CloV Then 
+                Throw New System.Exception("OAZO Interp - Invalid AppC")
+            ElseIf TypeOf func Is PrimV Then
+                Prim_Op_Function(DirectCast(func, PrimV).op, DirectCast(ast, AppC).args, env)
+            Else 
+                Throw New System.Exception("OAZO Interp - Invalid AppC")
+            End If
+        End If
+    End Function
+
+    Function Lookup(id As String, env as Environment) As Value
+        For Each bind As Bind In env.bindings
+            If bind.name = id Then 
+                Return bind.val
+            End If
+        Next
+        Throw New System.Exception("OAZO Lookup - Variable not found in environment")
+    End Function
+
+    Function Prim_Op_Function(op As String, args As List(Of ExprC), env As Environment) As Value
+        If args.Count = 2 Then
+            Dim left As NumV = Interp(args(0), env)
+            Dim right As NumV = Interp(args(1), env)
+
+            If op = "+" Then 
+                Return New NumV(left.num + right.num)
+            ElseIf op = "-" Then
+                Return New NumV(left.num - right.num)
+            ElseIf op = "*" Then
+                Return New NumV(left.num * right.num)
+            ElseIf op = "/" Then
+                If right.num = 0 Then 
+                    Throw New System.Exception("OAZO Prim-Op-Func Divide By Zero")
+                Else 
+                    Return New NumV(left.num / right.num)
+                End If
+            ElseIf op = "<=" Then
+                Return New BoolV(left.num <= right.num)
+            ElseIf op = "equal?" Then
+                Return New BoolV(left.num = right.num)
+            Else 
+                Throw New System.Exception("OAZO Prim-Op-Func Two Args ")
+            End If
+        ElseIf args.Count = 1 Then 
+            If op = "error" Then 
+                Throw New System.Exception("OAZO Error " & Serialize(Interp(args(0), env)))
+            Else 
+                Throw New System.Exception("OAZO Prim-Op-Func error ")
+            End If
+        Else 
+            If op = "true" Then 
+                Return New BoolV(True)
+            ElseIf op = "false" Then 
+                Return New BoolV(False)
+            Else 
+                Throw New System.Exception("OAZO Prim-Op-Func No Args No Match")
+            End If
+        End If
     End Function
     
     Sub Main()
@@ -183,6 +267,25 @@ Module VBModule
 
         ' TestSetTopEnv()
         ' TestSerialize()
+        ' TestInterp()
+        
+        Dim addition As AppC = New AppC(New IdC("+"), New List(Of ExprC)({New NumC(5), New NumC(5)}))
+        ' Dim additionVariable As AppC = New AppC(New LamC(New List(Of String)({"x", "y"}), New AppC(New IdC("+"), New List(Of ExprC)({New IdC("x"), New IdC("y")}))), New List(Of ExprC)({New NumC(5), New IdC(5)}))
+        ' Console.WriteLine(Serialize(Interp(addition, topenv)))
+    End Sub
+
+    Sub TestInterp()
+        Dim topenv As Environment = New Environment()
+        SetTopEnv(topenv)
+
+        Dim num_expr As NumC = New NumC(10)
+        Console.WriteLine(Serialize(Interp(num_expr, topenv)))
+
+        Dim id_expr As IdC = New IdC("+")
+        Console.WriteLine(Serialize(Interp(id_expr, topenv)))
+
+        Dim str_expr As StrC = New StrC("Hello World")
+        Console.WriteLine(Serialize(Interp(str_expr, topenv)))
     End Sub
 
     Sub TestSetTopEnv()
@@ -197,6 +300,9 @@ Module VBModule
     End Sub
 
     Sub TestSerialize() 
+        Dim topenv As Environment = New Environment()
+        SetTopEnv(topenv)
+
         Dim numval As NumV = New NumV(10)
         Dim strval As StrV = New StrV("Hello World")
         Dim trueval As BoolV = New BoolV(True)
@@ -205,7 +311,7 @@ Module VBModule
         listOfIds.Add("x")
         listOfIds.Add("y")
         listOfIds.Add("z")
-        Dim cloval As CloV = New CloV(listOfIds, New NumC(10))
+        Dim cloval As CloV = New CloV(listOfIds, New NumC(10), topenv)
         Dim primval As PrimV = New PrimV("+")
 
         Console.WriteLine(Serialize(numval))
